15d55110f3fad326fc6f85602baa3e3ad936f588
xelgand <gandalf.e2k@gmail.com>
2022-08-02 19:06:13 +0200

    fix warning for numeric option in random_string

--- terraform/compute/data.tf
+++ terraform/compute/data.tf
@@ -6,7 +6,7 @@ data "oci_identity_availability_domain" "ad" {
 resource "random_string" "cluster_token" {
   length           = 48
   special          = true
-  number           = true
+  numeric          = true
   lower            = true
   upper            = true
   override_special = "^@~*#%/.+:;_"
